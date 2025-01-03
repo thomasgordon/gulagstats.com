6a02793bfdd9ea33fda8a34a4d5344cfcb0c2b52
Tom Gordon <tomandrewgordon@gmail.com>
2025-01-03 15:22:04 +0000

    live fetching of api fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, inject } from '@angular/core';
 
 interface ResponseData {
   messageCount: number;
@@ -8,24 +10,24 @@ interface ResponseData {
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
+  private platformId = inject(PLATFORM_ID);
   data: number = 0;
-
   constructor(private http: HttpClient) {}
-
   private apiUrl = 'http://api.gulagstats.com/api/message_count';
 
   ngOnInit(): void {
-    this.http.get<ResponseData>(this.apiUrl).subscribe({
-      next: (response: ResponseData) => {
-        console.log('Success:', response);
-        this.data = response.messageCount;
-      },
-      error: (error) => console.error('Error:', error)
-    });
+    if (isPlatformBrowser(this.platformId)) {
+      this.http.get<ResponseData>(this.apiUrl).subscribe({
+        next: (response) => {
+          this.data = response.messageCount;
+        },
+        error: (error) => console.error('Error:', error)
+      });
+    }
   }
 }
