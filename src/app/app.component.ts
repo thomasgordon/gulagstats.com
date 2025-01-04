051de777084008252a59ae844bb9c999e7df0843
Tom Gordon <tomandrewgordon@gmail.com>
2025-01-04 20:34:51 +0000

    update site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { isPlatformBrowser } from '@angular/common';
 import { PLATFORM_ID, inject } from '@angular/core';
-
-interface ResponseData {
-  messageCount: number;
-}
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   private platformId = inject(PLATFORM_ID);
-  data: number = 0;
-  constructor(private http: HttpClient) {}
-  private apiUrl = 'http://api.gulagstats.com/api/message_count';
+
+  messageCount: number = 0;
+  topThreeMessagers: { user: string; messages: string }[] = [];
+
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      this.http.get<ResponseData>(this.apiUrl, {
-        headers: { 'Cache-Control': 'no-cache', 'Pragma': 'no-cache' }
-      }).subscribe({
+      // Fetch message count
+      this.dataService.getMessageCount().subscribe({
         next: (response) => {
-          this.data = response.messageCount;
+          this.messageCount = response.messageCount;
+          console.log('Message Count:', response);
         },
-        error: (error) => console.error('Error:', error)
+        error: (error) => console.error('Error fetching message count:', error),
+      });
+
+      // Fetch top three messagers
+      this.dataService.getTopThreeMessagers().subscribe({
+        next: (response) => {
+          this.topThreeMessagers = response.slice(0, 3);
+        },
+        error: (error) => console.error('Error fetching top three messagers:', error),
       });
     }
   }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,18 +3,35 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 interface ResponseData {
+  length: number;
   messageCount: number;
+  name: string;
+  array: [];
+}
+
+interface Messager {
+  user: string;
+  messages: string;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  private apiUrl = 'http://api.gulagstats.com/api/message_count';
+  private messageCountUrl = 'http://api.gulagstats.com/api/message_count';
+  private mostMessengersUrl = 'http://api.gulagstats.com/api/top_three_messagers';
 
   constructor(private http: HttpClient) {}
 
-  getData(): Observable<ResponseData> {
-    return this.http.get<ResponseData>(this.apiUrl);
+  getMessageCount(): Observable<ResponseData> {
+    return this.http.get<ResponseData>(this.messageCountUrl, {
+      headers: { 'Cache-Control': 'no-cache', 'Pragma': 'no-cache' },
+    });
+  }
+
+  getTopThreeMessagers(): Observable<Messager[]> {
+    return this.http.get<Messager[]>(this.mostMessengersUrl, {
+      headers: { 'Cache-Control': 'no-cache', 'Pragma': 'no-cache' },
+    });
   }
 }
